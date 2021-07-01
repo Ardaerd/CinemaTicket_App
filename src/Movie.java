120df9e94f451025c318a2cd7e1749502f35e0a8
arda.erdogan <arda.erdogan@ozu.edu.tr>
2021-07-01 09:59:53 +0300

    Movie class is initalized

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,17 @@
 public class Movie {
+    private String nameOfMovie;
+    private String genre;
+
+    public Movie(String nameOfMovie, String genre) {
+        this.nameOfMovie = nameOfMovie;
+        this.genre = genre;
+    }
+
+    public String getNameOfMovie() {
+        return nameOfMovie;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
