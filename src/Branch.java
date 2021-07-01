48b24cb8a177c824aa53d32e7f49c9c4e83e5023
arda.erdogan <arda.erdogan@ozu.edu.tr>
2021-07-01 09:56:33 +0300

    Branch's constructor is initalized

--- src/Branch.java
+++ src/Branch.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Branch {
+    private String nameOfBranch;
+    private ArrayList<Movie> listOfMovie;
+    private ArrayList<Ticket> prevTicket;
+
+    public Branch(String nameOfBranch) {
+        this.nameOfBranch = nameOfBranch;
+        listOfMovie = new ArrayList<>();
+        prevTicket = new ArrayList<>();
+    }
 }
--- src/MovieTheaterGroup.java
+++ src/MovieTheaterGroup.java
@@ -1,2 +1,4 @@
 public class MovieTheaterGroup {
+    // This class represent a movie theater group like Cinemaximum, Cinecity
+    private String nameOfGroup;
 }
