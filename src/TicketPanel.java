2b4646a0dc06bf6856fad24032948526e07cede3
Ardaerd <arderd@outlook.com>
2021-07-03 01:05:00 +0300

    TicketPanel is initalized

--- src/TicketPanel.java
+++ src/TicketPanel.java
@@ -1,12 +1,18 @@
 import Model.Ticket;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TicketPanel extends JPanel {
     private JPanel seatPanel;
     private JPanel ticketPanel;
 
     public TicketPanel() {
+        // Initializing  hte components
+        seatPanel = new JPanel();
+        ticketPanel = new JPanel();
 
+        // Creating layout for seat panel
+        seatPanel.setLayout(new GridBagLayout());
     }
 }
