59dcaeecc0cc332aa6936a804bb0e6cc25f88f1b
Ardaerd <arderd@outlook.com>
2021-07-02 17:38:11 +0300

    NewCustomer_Panel is done

--- src/CinemaTicket_GUI.java
+++ src/CinemaTicket_GUI.java
@@ -3,33 +3,52 @@ import Model.MovieTheaterGroup;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CinemaTicket_GUI extends JPanel {
-    private JPanel CinemaximumTaksim_Panel;
-    private JPanel CinemaximumLevent_Panel;
-    private JPanel CinemaximumAtaşehir_Panel;
     private JButton newCustomer;
     private JButton existingCustomer;
+    private TicketPanel ticketPanel;
     private JTabbedPane tabPanel;
     private MovieTheaterGroup movieTheaterGroup;
+    private JPanel buttonPanel;
+    private NewCustomer_Panel newCustomer_Panel;
 
     public CinemaTicket_GUI() {
         // Initializing components
-        CinemaximumAtaşehir_Panel = new CinemaximumAtaşehir_Panel();
-        CinemaximumTaksim_Panel = new CinemaximumTaksim_Panel();
-        CinemaximumLevent_Panel = new CinemaximumLevent_Panel();
         tabPanel = new JTabbedPane();
         movieTheaterGroup = new MovieTheaterGroup("Cinemaximum");
+        ticketPanel = new TicketPanel();
+        newCustomer = new JButton("New Customer");
+        existingCustomer = new JButton("Existing Customer");
+        buttonPanel = new JPanel();
+        newCustomer_Panel = new NewCustomer_Panel(movieTheaterGroup);
+
+        // Adding components to the buttonPanel
+        buttonPanel.add(newCustomer);
+        buttonPanel.add(existingCustomer);
+
+        // Adding actionListener to the components of the buttonPanel
+        newCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() instanceof JButton) {
+                    tabPanel.remove(buttonPanel);
+                    tabPanel.addTab("Ticket",newCustomer_Panel);
+                    tabPanel.repaint();
+                    tabPanel.revalidate();
+                }
+            }
+        });
 
         // Setting size
         tabPanel.setPreferredSize(new Dimension(main.WIDTH-20,main.HEIGHT-50));
 
         // Adding branch to the tab from the branch class
-        tabPanel.addTab(movieTheaterGroup.getListOfBranch().get(0).getNameOfBranch(), CinemaximumAtaşehir_Panel);
-        tabPanel.addTab(movieTheaterGroup.getListOfBranch().get(1).getNameOfBranch(),CinemaximumTaksim_Panel);
-        tabPanel.addTab(movieTheaterGroup.getListOfBranch().get(2).getNameOfBranch(),CinemaximumLevent_Panel);
+        tabPanel.addTab("Ticket",buttonPanel);
 
-
+        // Adding components to the this panel
         add(tabPanel);
     }
 }
---
+++ src/NewCustomer_Panel.java
@@ -0,0 +1,88 @@
+import Model.Customer;
+import Model.MovieTheaterGroup;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NewCustomer_Panel extends JPanel {
+    private JLabel firstNameLabel;
+    private JLabel lastNameLabel;
+    private JTextField firstName;
+    private JTextField lastName;
+    private JButton addButton;
+    private GridBagConstraints gbc;
+    private MovieTheaterGroup movieTheaterGroup;
+    private JPanel addCustomer_Panel;
+    private TicketPanel ticketPanel;
+
+    public NewCustomer_Panel(MovieTheaterGroup movieTheaterGroup) {
+        // Initializing components of the newCustomer_Panel
+        addCustomer_Panel = new JPanel();
+        ticketPanel = new TicketPanel();
+        firstNameLabel = new JLabel("First Name: ");
+        lastNameLabel = new JLabel("Last Name: ");
+        firstName = new JTextField();
+        lastName = new JTextField();
+        addButton = new JButton("Add");
+        gbc = new GridBagConstraints();
+        this.movieTheaterGroup = movieTheaterGroup;
+
+        // Setting layout for this panel
+        addCustomer_Panel.setLayout(new GridBagLayout());
+
+        // Setting place for the components of the newCustomer_Panel
+        gbc.gridheight = 1;
+        gbc.gridwidth = 1;
+
+        gbc.insets = new Insets(10,10,10,10);
+
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        addGB(firstNameLabel,0,0);
+
+        gbc.ipadx = 190;
+        gbc.ipady = 5;
+
+        gbc.anchor = GridBagConstraints.FIRST_LINE_END;
+        addGB(firstName,1,0);
+
+        gbc.insets = new Insets(10,10,25,10);
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        addGB(lastNameLabel,0,1);
+
+        gbc.anchor = GridBagConstraints.FIRST_LINE_END;
+        addGB(lastName,1,1);
+
+        gbc.insets = new Insets(10,10,200,10);
+        gbc.gridwidth = 2;
+        gbc.ipadx = 250;
+        gbc.ipady = 10;
+        gbc.anchor = GridBagConstraints.CENTER;
+        addGB(addButton,0,2);
+
+        // Adding actionListeners to the add button
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() instanceof JButton) {
+                    movieTheaterGroup.addCustomer(new Customer(firstName.getText(),lastName.getText()));
+                    JOptionPane.showMessageDialog(null,firstName.getText().toUpperCase() + " " + lastName.getText().toUpperCase() + " is added", "Customer",JOptionPane.INFORMATION_MESSAGE);
+                    remove(addCustomer_Panel);
+                    add(ticketPanel);
+                    revalidate();
+                    repaint();
+                }
+            }
+        });
+
+        add(addCustomer_Panel);
+    }
+
+    // Creating function to using GridBagLayout easily
+    public void addGB(Component comp, int x, int y) {
+        gbc.gridx = x;
+        gbc.gridy = y;
+        addCustomer_Panel.add(comp,gbc);
+    }
+}
